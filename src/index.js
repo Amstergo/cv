0b30970f0dcb0ba074b4c9dee718d325f86cccc8
amstergo <amstergo46@gmail.com>
2018-07-29 20:46:25 +0300

    feat: added loaders(css/images/fonts)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import "./css/style.css";
+
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add("hello");
+
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to "].join("\n\n");
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
   },
-	devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000
   }
